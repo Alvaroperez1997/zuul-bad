f1a730e9507d4ea56df9e6a19e78611f263576b0
Alvaro <alvaroperbay97@gmail.com>
2016-05-18 12:17:05 +0200

    Añadida clase main del juego

---
+++ WorldOfZuul.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class WorldOfZuul here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class WorldOfZuul
+{
+    /**
+     * Metodo main
+     */
+    public static void main(String[] args)
+    {
+        Game juego = new Game();
+        juego.play();
+    }
+}
